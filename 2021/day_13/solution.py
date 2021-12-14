33554eccb463a6beb90f2577e281808fc86efba4
attila.szombati <attila.szombati@lensa.com>
2021-12-14 13:35:26 +0100

    create gif

--- 2021/day_13/solution.py
+++ 2021/day_13/solution.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import moviepy.editor as moviepy
 import numpy as np
 
 
@@ -45,14 +46,34 @@ def solutions():
         matrix = [[0 for _ in range(get_max_y(char_input) + 1)] for _ in range(get_max_x(char_input) + 1)]
         for i, j in char_input:
             matrix[i][j] = 1
+        filenames = []
         for idx, fold in enumerate(folding):
             next_matrix = next_folding(new_m=matrix, fold=fold)
             matrix = next_matrix
             if idx == 0:
                 solution1 = count_hashtags(matrix=matrix)
+            np_arr = np.array(matrix)
+            filenames.append(f"solution{idx}.png")
+            plt.imsave(f'solution{idx}.png', np_arr)
 
-        np_arr = np.array(matrix)
-        plt.imsave('solution2.png', np_arr)
+        def resize_func(t):
+            if t <= 3:
+                return 1 + 0 * t  # Zoom-in.
+            elif 4 <= t <= 6:
+                return 1 + 0.2 * t  # Stay.
+            elif 7 <= t <= 8:
+                return 1 + 0.4 * t  # Stay.
+            else:
+                return 1 + 0.8 * t  # Zoom-out.
+
+        def make_gif():
+            clips = [moviepy.ImageClip(i).set_duration(0.5).resize(resize_func(idx))
+                     for idx, i in enumerate(filenames)]
+            concat_clip = moviepy.concatenate_videoclips(clips, method="compose")
+            concat_clip.write_gif("test.gif", fps=2)
+
+        make_gif()
+
         return solution1
 
 
